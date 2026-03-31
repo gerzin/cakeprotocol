f3713392fc4d81bdafd4701d0088671bbf4f57fe
Gerardo Zinno <zinno.gerardo.95@gmail.com>
2026-03-31 02:07:00 +0200

    Add argparse

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,6 +1,19 @@
 #include "application.hpp"
+#include <argparse/argparse.hpp>
 #include <cstdlib>
-auto main() -> int {
+#include <filesystem>
+#include <spdlog/spdlog.h>
+
+auto main(int argc, char **argv) -> int {
+  argparse::ArgumentParser args{std::filesystem::path{argv[0]}.stem().string()};
+
+  try {
+    args.parse_args(argc, argv);
+  } catch (const std::exception &err) {
+    spdlog::error("Error: {}", err.what());
+    return EXIT_FAILURE;
+  }
+
   Application app;
   app.run();
 
