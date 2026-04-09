4fffe209c946f40b36b1af8566c6cf9d873e686d
Gerardo Zinno <zinno.gerardo.95@gmail.com>
2026-04-10 00:54:20 +0200

    Store frames for debug

--- src/away_detector/haar_cascade_strategy.cpp
+++ src/away_detector/haar_cascade_strategy.cpp
@@ -3,6 +3,11 @@
 #include <opencv2/objdetect.hpp>
 #include <opencv2/videoio.hpp>
 #include <spdlog/spdlog.h>
+#ifdef CAKE_DEBUG_VIZ
+#include <filesystem>
+#include <fmt/format.h>
+#include <opencv2/imgcodecs.hpp>
+#endif
 
 namespace cake::away_detector::strategies {
 
@@ -40,6 +45,17 @@ auto HaarCascadeStrategy::is_away() const noexcept -> bool {
 
   std::vector<cv::Rect> faces;
   m_cascade.detectMultiScale(gray, faces);
+
+#ifdef CAKE_DEBUG_VIZ
+  for (const auto &face : faces) {
+    cv::rectangle(frame, face, cv::Scalar(0, 255, 0), 2);
+  }
+  constexpr auto debug_dir = "/tmp/cakeprotocol";
+  std::filesystem::create_directories(debug_dir);
+  auto path = fmt::format("{}/frame_{:04d}.png", debug_dir, m_frame_counter++);
+  cv::imwrite(path, frame);
+  spdlog::debug("Debug frame saved to {}", path);
+#endif
 
   if (faces.empty()) {
     ++m_consecutive_misses;
